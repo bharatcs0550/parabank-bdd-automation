fb4fc70d50f676f1f32746194be2ce8a34525eae
bharatcs0550 <bharatcs0550@gmail.com>
2026-07-03 22:26:50 +0530

    chore: initialize project with Playwright and playwright-bdd setup

---
+++ playwright.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require('@playwright/test');
+const { defineBddConfig } = require('playwright-bdd');
+
+const testDir = defineBddConfig({
+  features: 'features/*.feature',
+  steps: 'steps/*.js',
+});
+
+module.exports = defineConfig({
+  testDir,
+  reporter: [['html', { open: 'never' }], ['list']],
+  use: {
+    baseURL: 'https://parabank.parasoft.com',
+    headless: false,          
+    screenshot: 'on',          
+    trace: 'on',               
+  },
+});
